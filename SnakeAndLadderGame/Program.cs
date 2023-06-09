a3b87dadff00ae97b0c8fa3c01cf49c7be0e33b6
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-09 17:41:22 +0530

    [Ritvik]Add: Welcome Message

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadderGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+        }
+    }
+}
