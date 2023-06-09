b62c56d0e58b2dbb1479260cc2aa666a207267bd
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-09 17:45:46 +0530

    [Ritvik]Add: Player one at start position 0

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake And Ladder Game");
+            Console.WriteLine("Welcome to Snake And Ladder Game\n");
+
+            int position = 0;
+            int playerOne;
+            playerOne = position;
+            Console.WriteLine("Position of PlayerOne is : " + playerOne +"\n");
         }
     }
 }
